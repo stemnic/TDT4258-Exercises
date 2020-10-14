cf2a652563046c1a4601ff49005bb4b7007f5161
Ole Sivert <ole.sivert@aarhaug.com>
2020-10-14 10:06:34 +0200

    removed unneeded pull again

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Docs can be found https://hub.docker.com/r/ludvighz/efm32gg-devkit
 
-docker pull ludvighz/efm32gg-devkit:codesourcery
-
 docker run -it --privileged -v $(pwd):/work -v /dev/bus/usb:/dev/bus/usb ludvighz/efm32gg-devkit:codesourcery
 
