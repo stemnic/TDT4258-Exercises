34975c0bb307d9d5608eeb55949392d525531f48
Audun <audun.asdal@gmail.com>
2020-10-14 12:41:53 +0200

    Removed polling dir

--- ex2polling/dac.h
+++
@@ -1,8 +0,0 @@
-#ifndef DAC_H
-#define DAC_H
-
-void dac_config(void);
-void dac_write(unsigned short l, unsigned short r);
-void dac_start(void);
-void dac_stop(void);
-#endif
--- ex2polling/gpio.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPIO_H
-#define GPIO_H
-
-void gpio_config(void);
-void led_set(unsigned char led, unsigned char state);
-unsigned char get_button(unsigned char btn);
-
-#endif
